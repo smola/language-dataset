df7b1d9bd24938ced5090804579842947ce7726e
Santiago M. Mola <santi@mola.io>
2020-09-19 20:21:57 +0200

    Add weighted voting (#99)
    Weight each (auto)annotator with its precision.

--- tools/vote.py
+++ tools/vote.py
@@ -1,7 +1,7 @@
 from .common import *
 
 
-def vote(ann):
+def vote(ann, weights):
     all_votes = {}
     human_votes = {}
     for key, lang in ann.items():
@@ -9,7 +9,9 @@ def vote(ann):
             continue
         if key.startswith("human"):
             human_votes[lang] = human_votes.get(lang, 0) + 1
-        all_votes[lang] = all_votes.get(lang, 0) + 1
+            all_votes[lang] = all_votes.get(lang, 0.0) + 1.0
+        else:
+            all_votes[lang] = all_votes.get(lang, 0.0) + weights.get(key, 1.0)
     if len(human_votes) > 0:
         max_votes = max(human_votes.values())
         langs = [l for l, v in human_votes.items() if v == max_votes]
@@ -17,19 +19,50 @@ def vote(ann):
             return langs[0]
     max_votes = max(all_votes.values())
     langs = [l for l, v in all_votes.items() if v == max_votes]
-    if len(langs) == 1:
-        return langs[0]
-    return "Unknown"
+    if len(langs) != 1:
+        return "Unknown"
+    lang = langs[0]
+    if all_votes[lang] <= sum(all_votes.values()) - all_votes[lang]:
+        return "Unknown"
+    return lang
 
 
-def vote_all(dataset):
+def vote_all(dataset, weights):
     for path, file_data in dataset.data["files"].items():
-        file_data["annotations"]["vote"] = vote(file_data["annotations"])
+        file_data["annotations"]["vote"] = vote(file_data["annotations"], weights)
+
+
+def get_weights(dataset):
+    good = {}
+    bad = {}
+    for path, file_data in dataset.data["files"].items():
+        ann = file_data["annotations"]
+        if not any([k.startswith("human-") for k in ann.keys()]):
+            continue
+        class_ = file_data["annotations"]["vote"]
+        if class_ == "Unknown":
+            continue
+        for k, v in ann.items():
+            if k.startswith("human-"):
+                continue
+            if k == "vote":
+                continue
+            if v == class_:
+                good[k] = good.get(k, 0) + 1
+            else:
+                bad[k] = bad.get(k, 0) + 1
+    weights = {}
+    for k in set(good.keys()) | set(bad.keys()):
+        weights[k] = good.get(k, 0) / (good.get(k, 0) + bad.get(k, 0))
+    return weights
 
 
 def main():
     dataset = Dataset()
-    vote_all(dataset)
+    weights = get_weights(dataset)
+    for k, v in sorted(weights.items()):
+        print(f"Weight: {k} -> {v}")
+    vote_all(dataset, weights)
     dataset.save()
 
 
