ced503377dd9d2e2041b2ed6ab9de6c5aeb3d943
Santiago M. Mola <santi@mola.io>
2020-09-25 18:35:52 +0200

    Update linguist annotations to v7.11.0 (#111)
    * Update linguist annotations to v7.11.0
    * Improve performance of linguist annotations
    * Linguist languages de-grouped and now without .gitattributes

--- tools/linguist_wrapper.rb
+++ tools/linguist_wrapper.rb
@@ -3,23 +3,30 @@
 require 'json'
 require 'linguist'
 
-path = ARGV[0]
-
-blob = Linguist::FileBlob.new(path)
-
 res = {}
+ARGV.each do |path|
+  path = ARGV[0]
+  blob = Linguist::FileBlob.new(path)
+  res[path] = {}
 
-res['modeline'] = Linguist::Strategy::Modeline.call(blob, [])
-res['filename'] = Linguist::Strategy::Filename.call(blob, [])
-res['shebang'] = Linguist::Shebang.call(blob, [])
-res['extension'] = Linguist::Strategy::Extension.call(blob, [])
-res['xml'] = Linguist::Strategy::XML.call(blob, [])
-res['manpage'] = Linguist::Strategy::Manpage.call(blob, [])
-res['heuristics'] = Linguist::Heuristics.call(blob, res['extension'])
-#begin
-#    res['classifier-ext'] = Linguist::Classifier.call(blob, res['extension'])
-#rescue
-#    #TODO: https://github.com/github/linguist/issues/4995
-#end
+  l = Linguist.detect(blob)
+  if l.nil?
+    res[path]['all'] = []
+  else
+    res[path]['all'] = [l]
+  end
 
+  res[path]['modeline'] = Linguist::Strategy::Modeline.call(blob, [])
+  res[path]['filename'] = Linguist::Strategy::Filename.call(blob, [])
+  res[path]['shebang'] = Linguist::Shebang.call(blob, [])
+  res[path]['extension'] = Linguist::Strategy::Extension.call(blob, [])
+  res[path]['xml'] = Linguist::Strategy::XML.call(blob, [])
+  res[path]['manpage'] = Linguist::Strategy::Manpage.call(blob, [])
+  res[path]['heuristics'] = Linguist::Heuristics.call(blob, res[path]['extension'])
+  #begin
+  #    res[path]['classifier-ext'] = Linguist::Classifier.call(blob, res['extension'])
+  #rescue
+  #    #TODO: https://github.com/github/linguist/issues/4995
+  #end
+end
 puts JSON.generate(res)
